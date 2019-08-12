af0aaefbc0475dfcf812fb6050d3a674171c4cb1
florianml <florian.laplanche@gmail.com>
2019-08-12 01:42:57 -0400

    Updates

---
+++ precache-manifest.80c832514d863fdc3ed74f09ee00173d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992d3c9389e2d5c879b3c5a9ed5bbdef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d0e7a836a93351b2852",
+    "url": "/static/css/main.434ab049.chunk.css"
+  },
+  {
+    "revision": "ced1aa6a9177909feb9c",
+    "url": "/static/js/2.715e5857.chunk.js"
+  },
+  {
+    "revision": "6d0e7a836a93351b2852",
+    "url": "/static/js/main.23460ac3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
